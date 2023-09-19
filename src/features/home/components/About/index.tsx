ca4cf1059af257472d8a19c5b40eed8518af3d6c
DESKTOP-4ON72SE\INTEL <owen@madeindonesia.com>
2023-09-19 13:14:18 +0700

    change banner photo

--- src/features/home/components/About/index.tsx
+++ src/features/home/components/About/index.tsx
@@ -1,27 +1,24 @@
 import Image from "next/image";
-import React from "react";
 import s from "../../view/styles.module.scss";
 
-
 const About = () => {
-    return ( 
-        <div className={s.AboutSection}>
-            <div className={s.AboutWrapper}>
-                <div className={s.ImageWrapper}>
-                    <Image
-                        width={1200}
-                        height={1200}
-                        src="/images/mines/HeroBanner.jpg"
-                        alt="Hero banner"
-                        priority
-                        loading="eager"
-                        
-                    />
-                </div>
-                <div className={s.summaryWrapper}></div>
-            </div>
-        </div>
-     );
+	return (
+		<div className={s.AboutSection}>
+			<div className={s.AboutWrapper}>
+				<div className={s.ImageWrapper}>
+					<Image
+						width={2560}
+						height={1200}
+						src="/images/mines/HeroBanner.webp"
+						alt="Hero banner"
+						priority
+						loading="eager"
+					/>
+				</div>
+				<div className={s.summaryWrapper}></div>
+			</div>
+		</div>
+	);
 };
- 
+
 export default About;
