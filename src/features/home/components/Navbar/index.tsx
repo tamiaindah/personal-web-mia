06df9c99be3f55d0a3748c1a32e7c352ff9838d7
tamiaindah <tamiaindah66000@gmail.com>
2023-09-19 17:39:31 +0700

    update again

--- src/features/home/components/Navbar/index.tsx
+++ src/features/home/components/Navbar/index.tsx
@@ -1,54 +1,58 @@
 import { socialMediaLinks } from "@utils/socialMediaLinks";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
 import s from "../../view/styles.module.scss";
-
 
 // const handleClick = () => {
 //   console.log("hello, mia");
 // };
 
 const Navbar = () => {
-
-  return (
-    <nav className={s.navbar}>
-      <div className={s.container}>
-        <div className={s.navbarSection}>
-          <div className={s.burger}>
-          <Link href="/">
-            <Image
-							width={42}
-							height={42}
-							src="/images/icons/bar-dark.svg"
-							alt="Navbar-icon"
-							priority
-							loading="eager"
-						/>
-            </Link>
-          </div>
-          <div className={s.links}>
-            <Link href={socialMediaLinks.github} target="_blank" title="Github">github</Link>
-            <Link href={socialMediaLinks.dribbble} target="_blank" title="Dribbble">dribbble</Link>
-            <Link href={socialMediaLinks.linkedin} target="_blank" title="Linkedin">linkedin</Link>
-          </div>
-          <div className={s.logo}>
-            <Link href="/">
-            <Image
-							width={81}
-							height={42}
-							src="/images/logos/logo-dark.svg"
-							alt="Mia-logo"
-							title="Personal Website Mia"
-							priority
-							loading="eager"
-						/>
-            </Link>
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
+	return (
+		<nav className={s.navbar}>
+			<div className={s.container}>
+				<div className={s.navbarSection}>
+					<div className={s.burger}>
+						<Link href="/">
+							<Image
+								width={42}
+								height={42}
+								src="/images/icons/bar-dark.svg"
+								alt="Navbar-icon"
+								priority
+								loading="eager"
+								className="burger__trigger"
+							/>
+						</Link>
+					</div>
+					<div className={s.links}>
+						<Link href={socialMediaLinks.github} target="_blank" title="Github">
+							github
+						</Link>
+						<Link href={socialMediaLinks.dribbble} target="_blank" title="Dribbble">
+							dribbble
+						</Link>
+						<Link href={socialMediaLinks.linkedin} target="_blank" title="Linkedin">
+							linkedin
+						</Link>
+					</div>
+					<div className={s.logo}>
+						<Link href="/">
+							<Image
+								width={81}
+								height={42}
+								src="/images/logos/logo-dark.svg"
+								alt="Mia-logo"
+								title="Personal Website Mia"
+								priority
+								loading="eager"
+							/>
+						</Link>
+					</div>
+				</div>
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
